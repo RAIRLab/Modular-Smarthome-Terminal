79f487332e4171319753258d6b269f6477772a47
b-dugan <BeatriceDugan652@gmail.com>
2026-02-24 02:35:55 -0500

    Add start.py Layout and Templates

--- flaskServer.py
+++ flaskServer.py
@@ -1,5 +1,8 @@
-from flask import Flask
+# flaskServer.py
+# Responsible for running the internal flask server that handles displaying the UI to the user
 
+from flask import Flask
+import time
 
 app = Flask(__name__)
 
@@ -7,6 +10,17 @@ app = Flask(__name__)
 def clientHome():
     return "<center><h1>Hello This is the placeholder for the <b>index.html</b></h1></center>" #render_template("clientHome.html")
 
-
+# Slated for removal-- starting through start.py now
 if __name__ == '__main__':
     app.run(debug=True)
+
+def thread():
+    setup()
+    run()
+
+def setup():
+    app.run(debug=True)
+
+def run():
+    print("Flask server running...")
+    time.sleep(5)
